0f4c0952e1c5591ac1146a20b5863529b59228b0
Ahmed-Moudud <suzan.4bd@gmail.com>
2022-10-25 23:46:25 +0600

    api for individual course is ready

--- index.js
+++ index.js
@@ -14,7 +14,14 @@ app.get("/allcourses", (req, res) => {
     res.send(coursesCollection)
 });
 
-
+app.get("/course/:id", (req, res) => {
+    const id = req.params.id;
+    const getCoursesDetail = coursesCollection?.find((c) => c.id == id);
+    if(!getCoursesDetail){
+        res.send("not found");
+    }
+    res.send(getCoursesDetail);
+})
 
 app.listen(Port, () => {
     console.log('server is running on', Port)
